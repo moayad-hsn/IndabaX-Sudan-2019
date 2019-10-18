304ee2ef45b0518f6ed4dbf8990d9bbf1333b9f7
AmrKhalifa <amr.khalifa.m.a@gmail.com>
2019-10-18 21:55:34 +0200

    organized imports

--- Pytorch FeedForward nets/colors.py
+++ Pytorch FeedForward nets/colors.py
@@ -5,3 +5,8 @@ GREEN = "\033[0;32m"
 RESET = "\033[0;0m"
 BOLD    = "\033[;1m"
 REVERSE = "\033[;7m"
+
+def print_colored_line(color): 
+    sys.stdout.write(color)
+    print("*="*35)
+    sys.stdout.write(colors.RESET)
---
+++ Pytorch FeedForward nets/imports.py
@@ -0,0 +1,9 @@
+import colors
+from os import sys
+import random
+import torch
+from torch import nn, optim
+import math
+from IPython import display
+from plot_lib import plot_data, plot_model, set_default
+import torch.functional as F
